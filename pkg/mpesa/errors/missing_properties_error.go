2d8b5a76f77ea69225fbd692cbc134a9be8a041b
Edson Michaque <edson@michaque.com>
2020-10-10 16:38:09 +0200

    Add validation error types

---
+++ pkg/mpesa/errors/missing_properties_error.go
@@ -0,0 +1,18 @@
+package errors
+
+import "fmt"
+
+type MissingPropertiesError struct {
+	missingProperties []string
+}
+
+func NewMissingPropertiesError(props []string) *ValidationError {
+	return &MissingPropertiesError{
+		missingProperties: props
+	}
+}
+
+func (v *MissingPropertiesError) Error() string {
+	return fmt.Sprintf("The following fields have uninitialized values: %s", v.missingProperties)
+}
+
